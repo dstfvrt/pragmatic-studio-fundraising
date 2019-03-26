3db8b15739c6b84620dba06278de593d9a0f2afc
Dustin Favorite <dstfvrt@gmail.com>
2019-03-26 09:32:33 -0500

    Added 'fundraising.rb' main file, puts project names and funding

---
+++ lib/fundraising.rb
@@ -0,0 +1,9 @@
+project_1 = "Project Alpha"
+project_1_funding = 1000000000
+project_2 = "Project Beta"
+project_2_funding = 2
+project_3 = "Project Theta"
+project_3_funding = 1
+
+puts "#{project_1} has $#{project_1_funding} in funding."
+puts "Projects:\n#{project_1}\n#{project_2}\n#{project_3}"
