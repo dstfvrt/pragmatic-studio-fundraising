53eec3dbab00ee012942091173af20658f310465
Dustin Favorite <dstfvrt@gmail.com>
2019-04-11 08:50:16 -0500

    WIP

--- lib/fund_manager.rb
+++ lib/fund_manager.rb
@@ -1,6 +1,6 @@
 require_relative 'project'
 require_relative 'funding_round'
-
+require 'pry'
 class Hedgefund
   attr_reader :co_name, :projects
 
@@ -19,9 +19,35 @@ class Hedgefund
     projects
   end
 
-  def update
-    @projects.each do |project|
-      FundingRound.update(project)
+  def update(days)
+
+    1.upto(days) do |d|
+      puts "Day #{d}:"
+      @projects.each do |project|
+        FundingRound.update(project)
+      end
+    end
+
+    under_funded, fully_funded = projects.partition { |p| p.funds_needed >= 0}
+
+    if fully_funded.size == 0
+      puts "No projects are fully funded."
+    else
+      puts "Fundraising completed.\nFully funded projects:"
+      fully_funded.each do |p|
+        puts "#{p.name}, #{p.funding} out of #{p.funding_target}"
+      end
+    end
+
+    if under_funded.size == 0
+      puts "No projects are in need of further contributions."
+    else
+      binding.pry
+      under_funded.sort_by { |a, b| a.funds_needed <=> b.funds_needed}
+      puts "Projects in need of contributions:"
+      under_funded.each do |p|
+        puts "#{p.name}, #{p.funding} out of #{p.funding_target}"
+      end
     end
   end
 end
