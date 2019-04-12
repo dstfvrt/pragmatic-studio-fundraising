935a5929462ddf75e0427387504d3c70e3856eb9
Dustin Favorite <dstfvrt@gmail.com>
2019-04-12 08:20:04 -0500

    Add 'blocks Added blocks to iterate through projects

--- lib/fund_manager.rb
+++ lib/fund_manager.rb
@@ -1,6 +1,5 @@
 require_relative 'project'
 require_relative 'funding_round'
-require 'pry'
 class Hedgefund
   attr_reader :co_name, :projects
 
@@ -42,8 +41,11 @@ class Hedgefund
     if under_funded.size == 0
       puts "No projects are in need of further contributions."
     else
-      binding.pry
-      under_funded.sort_by { |a, b| a.funds_needed <=> b.funds_needed}
+      under_funded.sort_by do |first, second|
+        return first unless second
+        first <=> second
+      end
+
       puts "Projects in need of contributions:"
       under_funded.each do |p|
         puts "#{p.name}, #{p.funding} out of #{p.funding_target}"
