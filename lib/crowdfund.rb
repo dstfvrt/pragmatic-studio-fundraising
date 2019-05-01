0efe3b5102646de5c9f7a4a76a658d0fcc9b0fd8
Dustin Favorite <dstfvrt@gmail.com>
2019-05-01 08:31:40 -0500

    Added 'Custom Iterators' Funding rounds are now user controlled, end of funding results out put to file

--- lib/crowdfund.rb
+++ lib/crowdfund.rb
@@ -6,11 +6,26 @@ beta = Project.new("beta")
 gamma = Project.new("gamma", 400, 1200)
 
 favorite_funding = Hedgefund.new("Favorite LLC")
+=begin
 favorite_funding.add_project(alpha)
 favorite_funding.add_project(beta)
 favorite_funding.add_project(gamma)
+=end
 
-favorite_funding.status;
+favorite_funding.load_projects("projects.csv")
+loop do
+  puts "Please enter days of funding: ('e' to exit)"
+  answer = gets.chomp
+
+  case answer
+  when /^\d+$/
+    favorite_funding.update(answer.to_i)
+  when /[e]/
+    break
+  else
+    puts "Please enter a number or 'e' to exit."
+  end
+end
 
 favorite_funding.projects.each_with_index do |project, index|
   if project.funds_needed > 0
@@ -19,4 +34,4 @@ favorite_funding.projects.each_with_index do |project, index|
   end
 end
 
-puts favorite_funding.status
+favorite_funding.save_stats
